3a007dd21f1011859b180efa68140ef2226dcca1
yukkuri <nre10577@gmail.com>
2026-06-19 12:02:37 +0900

    Create firebase-admin.ts

---
+++ 食堂モバイルオーダー/cafeteria-order/src/lib/firebase-admin.ts
@@ -0,0 +1,20 @@
+import { initializeApp, getApps, cert, App } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase-admin/auth";
+
+let app: App;
+
+if (!getApps().length) {
+  app = initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+} else {
+  app = getApps()[0];
+}
+
+export const adminDb = getFirestore(app);
+export const adminAuth = getAuth(app);
